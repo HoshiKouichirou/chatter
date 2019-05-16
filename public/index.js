215df0eb8010965a5c19a6fccc642107868d009d
出川　大和 <hxdegawa@gmail.com>
2019-05-16 16:51:01 +0900

    Implemented all

---
+++ public/index.js
@@ -0,0 +1,19 @@
+$(()=>{
+	const db = firebase.firestore()
+	db.collection("rooms").doc("test").collection("messages").onSnapshot(function(snapshot){
+		snapshot.docChanges().forEach(function(change) {
+				if (change.type === "added") {
+          $("<li />").appendTo(".message")
+          $("<span />").text(change.doc.data().message).appendTo(".message > li:last")
+					$(".message").scrollTop($(".message")[0].scrollHeight)
+				};   
+		});
+});
+	$(".send").on("click", ()=>{
+		var message = $(".text").val()
+		db.collection("rooms").doc("test").collection("messages").add({
+			message:message
+		})
+	})
+	console.log(db)
+});
